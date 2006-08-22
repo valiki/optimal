550d91d9bb783124fd484094dfea2767a4e51ed6
bill.harlan <bill.harlan@99f2aad4-941b-0410-84ac-db151a327eb5>
2006-08-22 06:14:17 +0000

    These papers describe the design and use of this package.

---
+++ papers/inv/internals.pl
@@ -0,0 +1,42 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate internals original text with physical files.
+
+
+$key = q/eq:obj/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_luenberger/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gill/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_boxandtiao/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_harlan-model/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jaynes/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_menke/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:qobj/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_luenbergervec/;
+$ref_files{$key} = "$dir".q|inv.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
